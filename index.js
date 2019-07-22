ba7025df2c66640f06a8605e5aabed932031942d
Gal <yacobi.gal94@gmail.com>
2019-07-23 00:14:53 +0300

    Fixed end-game state and minor css tweaks
    Signed-off-by: Gal <yacobi.gal94@gmail.com>

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ let difficulties = {
     "medium": 1,
     "hard": 2
 };
+let wrongAns = "<div class='end'><h3 class='question'>Wrong answer!<br>Better luck next time</h3>" +
+               "<div class='container end-btns'><button class='yes-button'>Play Again</button></div></div>";
+let rightAns = "<h3 class='question end'>You Won!</h3><div class='container end-btns'><button class='yes-button'>Play Again</button></div>";
 
 //DOM
 categories.click(function () {
@@ -40,11 +43,9 @@ categories.click(function () {
                     return 0;
                 }
             });
-            $("#helper").fadeOut("400",function(){
-               $("#Game").fadeIn("500");
+            $("#helper").fadeOut(400,function(){
+                   setQuestion(data, 0);
             });
-
-            setQuestion(data, 0);
         })
         .catch(function () {
             console.log("Error");
@@ -65,41 +66,69 @@ $(".answers").click(function () {
         endGame(false)
     }
 });
+//resets Game
+$("body").on('click', '.yes-button' ,(function(){
+    score = 0;
+    i = 0;
+    $(".end").fadeOut(300, function(){
+        $(".score").text("Score: "+ score);
+        $("#helper").fadeIn(400);
+        $(".end").remove();
+    });
+}));
 
 //functions
 function random() {
     return Math.floor(Math.random() * 4);
 }
-
 //Set the question with 4 answers on the HTML page.
 function setQuestion(questionsBank, i) {
     let answerButtons = $(".answers");
     //Display the question on the HTML
     let question = questionsBank[i].question;
+    let answers = [];
     question = regexFix(question);
-    $(".question").text(question);
+    //$(".question").text(question);//
 
     //Set correct answer button and assign the other 3 buttons the incorrect answers.
     correctIndex = random();
-    answerButtons[correctIndex].textContent = regexFix(questionsBank[i].correct_answer);
+    answers[correctIndex] = regexFix(questionsBank[i].correct_answer);
+    //answerButtons[correctIndex].textContent = regexFix(questionsBank[i].correct_answer);//
     for (let j = 0, k = 0; j <= 3, k<3; j++) {
         if (j === correctIndex) {
             j++;
         }
-        answerButtons[j].textContent = regexFix(questionsBank[i].incorrect_answers[k]);
+        //answerButtons[j].textContent = regexFix(questionsBank[i].incorrect_answers[k]);//
+        answers[j] = regexFix(questionsBank[i].incorrect_answers[k]);
         k++;
     }
+    boardFade(question, answers);
 }
 //End Game scenario
 function endGame(state){
     if(state){
-        alert("You have won!")
+        $("#Game").fadeOut("300", function(){
+            $(".bg").after(rightAns);
+        });
     } else {
-        alert(":(")
+        $("#Game").fadeOut("300", function(){
+            $(".bg").after(wrongAns);
+        });
     }
 }
 //assign regex
 function regexFix(sentence){
     return sentence.replace(regB, '"').replace(regA, "'").replace(regB, '"')
         .replace(regC, '&').replace(regD, 'é').replace(regE, 'ö');
+}
+//Fade question and answers
+function boardFade(question, answers){
+    $("#Game").fadeOut("400", function(){
+        let answerButtons = $(".answers");
+        $(".question").text(question);
+        for (let i = 0; i < 4; i++){
+            answerButtons[i].textContent =answers[i];
+        }
+        $("#Game").fadeIn("400");
+    });
 }
